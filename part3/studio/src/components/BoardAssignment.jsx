485443c38353e9453d9d8f892a8c2597ccd894cf
aish.dande9 <aish.dande@gmail.com>
2024-09-05 20:28:15 -0500

    Co-authored-by: jamickael <jamickael@users.noreply.github.com> Co-authored-by: allisonRabia <allisonRabia@users.noreply.github.com>

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,69 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  let decadentDesserts = { label: "Decadent Desserts" };
+  let lateNightSnack = { label: "A Late Night Snack" };
+  let brunch = { label: "Brunch" };
+  const boards = [decadentDesserts, lateNightSnack, brunch];
+  const [boardName, setName] = useState(boards[0]);
 
-   const handleChange = (event) => {
-   }
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        {boards.map((data,id) => 
+          <option value = {data} key={id}>{data.label}</option>
+        )}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
+
+// boardName = "no boards yet!";
+
+//    const [myValue, setMyValue] = useState("");
+
+//    return (
+//      <div>
+//        <select
+//          onChange={(e) => setMyValue(e.target.value)}
+//          defaultValue={myValue}
+//        >
+//          <option>Option 1</option>
+//          <option>Option 2</option>
+//          <option>Option 3</option>
+//        </select>
+//        <h2>
+//          {" "}
+//          You selected{" "}
+//          <span style={{ backgroundColor: "yellow" }}>{myValue}</span>
+//        </h2>
+//      </div>
+//    );
+//  }
+// const options = ["options 1", "option 2", "option 3"];
+// const [myValue, setMyValue] = useState(options[0]);
+
+// return (
+//   <div>
+//     <select
+//       onChange={(e) => setMyValue(e.target.value)}
+//       defaultValue={myValue}
+//     >
+//       {options.map((option, idx) => (
+//         <option key={idx}>{option}</option>
+//       ))}
+//     </select>
+//     <h2>
+//       {" "}
+//       You selected{" "}
+//       <span style={{ backgroundColor: "yellow" }}>{myValue}</span>
+//     </h2>
+//   </div>
+// )
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import './components/App.css'
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://erickim.net/";
+   let authorPhoto = "https://images.squarespace-cdn.com/content/v1/55601ed9e4b0ef2fa3b5042b/5c9ae243-a746-43f3-9c5f-6576c4628638/eric-kim-nyt-cooking.png?format=1000w";
+   let authorName = "Eric Kim";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Eric Kim, chef, a young man in a kitchen preparing a plate from a tray of a dinner meal" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>The Strawberry King!</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["strawberries", "chamomile tea", "sugar", "flour", "eggs"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -36,8 +36,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Chamomile Tea Cake with Strawberry Icing</h1>
+            <p>The perfect cake recipe for afternoon tea. Make sure to host your gathering outside so everyone can get lots of springtime sunshine!</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +49,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://static01.nyt.com/images/2022/04/17/dining/17Mag-Eat1/17Mag-Eat1-threeByTwoMediumAt2X.jpg?width=1280&quality=75&auto=webp" alt="Chamomile Cake" className="imageUpdates"/>
    );
 }
 
